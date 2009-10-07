df1984a480870bc9d69738dc45476031f27f2a65
unknown <Ji Yan@.(none)>
2009-10-07 14:52:28 -0700

    added business model validation

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,9 @@
 class Business < ActiveRecord::Base
 	validates_presence_of :name
+	validates_presence_of :referal_program_name
 	validates_presence_of :description
+	validates_presence_of :referee_reward
+	validates_presence_of :category
 	has_many :contactrecords
 	has_many :referals
 	has_many :users, :through => :referals
